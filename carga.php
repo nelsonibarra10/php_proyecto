28fc2de582309e72cdb2cfe42c187aada0276b58
JorgeBsAs67 <jorgegonzalezfotografias@gmail.com>
2018-12-19 20:36:41 -0300

    dfdf
    sdfsdf

--- carga.php
+++ carga.php
@@ -21,8 +21,9 @@ $nombreArchivoDestino = $_FILES["imagen"]["name"];
 
 if (move_uploaded_file($_FILES["imagen"]["tmp_name"], $carpetaDestino."/".$nombreArchivoDestino)) {
 	// El archivo se subio ok
-	echo "Subido ok";
-	echo "El nombre original del archivo es ".$_FILES["imagen"]["name"];
+	//echo "Subido ok";
+    //echo "El nombre original del archivo es ".$_FILES["imagen"]["name"];
+    header('Location: segura.php');
 } else {
 	// Hubo un error en el upload
 	echo "Error en la subida del archivo";
